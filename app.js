39a49367af6c292c9fb59a9a8bb044437a11b6c9
NareshGorle <nareshgorle@>
2024-11-27 19:45:55 +0530

    web page

---
+++ app.js
@@ -0,0 +1,15 @@
+let log = document.getElementById("login");
+let reg = document.getElementById("registor");
+let btn = document.getElementById("btn");
+
+function registor(){
+    log.style.left = "-400px";
+    reg.style.left = "50px";
+    btn.style.left = "100px";
+};
+function login(){
+    log.style.left = "50px";
+    reg.style.left = "450px";
+    btn.style.left = "0";
+}
+
